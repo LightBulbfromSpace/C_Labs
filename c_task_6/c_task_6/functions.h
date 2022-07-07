2b1213e851eb89cd810d53ca6d5496ba68858f16
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-07-07 11:31:57 +0200

    Update functions.h

--- c_task_6/c_task_6/functions.h
+++ c_task_6/c_task_6/functions.h
@@ -1,5 +1,3 @@
 #pragma once
 
 int hopcost(char* str, int step);
-int hopcost_recursion(char* str, int step, int current_letter, int count,
-	int help_counter, int help_index, char last_cur_letter);
