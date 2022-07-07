6b34ceee1342b901455a94326ea75b207eea6ba4
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-07-07 10:20:52 +0200

    Update Calc.h

--- Revision_1/Revision_1/Calc.h
+++ Revision_1/Revision_1/Calc.h
@@ -2,7 +2,4 @@
 
 int Calc(char* exp);
 int EasiestCalc(char* exp);
-int Subcalc(char a, char symb, char b);
-char SymbChange(char symb);
-int NewSubcalc(char* num1, char symb, char* num2, _Bool NumCh);
 
